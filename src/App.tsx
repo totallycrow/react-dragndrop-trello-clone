4b816c90750e9425176d36de91ffb22d7781e2d7
Adrian Gajda <adrian.gajda@hotmail.com>
2022-10-16 20:41:03 +0100

    create UI Boards components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import { RootState } from "./app/store";
 import { useDispatch, useSelector } from "react-redux";
 
 import { IBoard } from "./slices/types";
+import BoardCard from "./components/BoardCard";
+import { TasksBoards } from "./components/TasksBoards";
+
+import { DndContext } from "@dnd-kit/core";
+
+// import { Draggable } from "./Draggable";
+// import { Droppable } from "./Droppable";
 
 // state -> board
 
@@ -17,6 +24,11 @@ import { IBoard } from "./slices/types";
 // boardContainer ->
 // boardsContainer : []
 
+// TODO:
+// Droppable Containers:
+// Draggable BoardCards
+// Draggable Tasks
+
 function App() {
   const boards = useSelector(
     (state: RootState) => state.boardsContainer.boards
@@ -25,11 +37,11 @@ function App() {
   return (
     <div className="bg-slate-400">
       <h1>Test</h1>
-      <h2>
-        {boards.map((board: any) => (
-          <div>{board.name}</div>
-        ))}
-      </h2>
+      <div>
+        <DndContext>
+          <TasksBoards />
+        </DndContext>
+      </div>
     </div>
   );
 }
---
+++ src/components/BoardCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function BoardCard() {
+  return (
+    <div className="h-40 w-40 bg-slate-500 m-2 p-2 inline-block">BoardCard</div>
+  );
+}
